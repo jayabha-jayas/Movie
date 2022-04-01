ba52888d2d64e7609958dcaea58071dfd9d1f186
jayabharathi-jb <jayabharathi.s@thoughtworks.com>
2022-04-01 12:26:22 +0530

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Jayabharathi S on 01/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

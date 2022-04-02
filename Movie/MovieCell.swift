0b44e0787cd1035d0600dee85da42323596ae639
jayabharathi-jb <jayabharathi.s@thoughtworks.com>
2022-04-02 15:35:43 +0530

    load name, description and rating from json

---
+++ Movie/MovieCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCell.swift
+//  Movie
+//
+//  Created by Jayabharathi S on 01/04/22.
+//
+
+import UIKit
+
+class MovieCell : UITableViewCell {
+    @IBOutlet weak var name : UILabel!
+    @IBOutlet weak var descriptionValue : UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet var logo: UIImageView!
+}
--- Movie/ViewController.swift
+++ Movie/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Movie{
+    var name: String!
+    var description: String!
+    var rating: Double!
+    var logo: String!
 }
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var movieList = [Movie]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        
+        fetchData(url: "https://tw-mobile-hiring.web.app/interview_ios.json") { movieData in
+            DispatchQueue.main.async {
+                self.movieList.append(contentsOf: movieData)
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func fetchData(url: String, completion: @escaping ([Movie]) -> Void) {
+        var movieData = [Movie]()
+        let urlValue = Foundation.URL(string: url)
+        let urlRequest = URLRequest(url: urlValue!)
+        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+        
+        urlSession.dataTask(with: urlRequest) { data, response, error in
+            let json = try? JSONSerialization.jsonObject(with: data!, options: .json5Allowed) as? [String : Any]
+            if let json = json {
+                if let result = json["data"] as? [String : Any]{
+                    if let cards = result["cards"] as? [[String : Any]]{
+                        for value in cards {
+                            if let val = value["content"] as? [String : Any]{
+                                movieData.append(Movie(name: val["title"] as? String, description: val["description"] as? String, rating: val["rating"] as? Double, logo: val["movie_logo"] as? String))
+                            }
+                        }
+                    }
+                }
+            }
+            completion(movieData)
+        }.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCellIdentifier") as! MovieCell
+        let movie = movieList[indexPath.row]
+        cell.name.text = movie.name
+        cell.name.numberOfLines = 0
+        cell.descriptionValue.text = movie.description
+        cell.descriptionValue.numberOfLines = 0
+        cell.descriptionValue.sizeToFit()
+        cell.descriptionValue.showsExpansionTextWhenTruncated = true
+        cell.rating.text = String(movie.rating)
+//        let url = URL(string: movie.logo)
+//        let data = try? Data(contentsOf: url!)
+//        let uiImage = UIImage(data: data!)
+//        cell.logo = UIImageView(image: uiImage)
+        return cell
+    }
+}
